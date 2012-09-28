4a13503fbe3f6859671c11115b294904e756e12d
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-09-29 01:24:58 +0200

    WYSIWYG editing for news.
    And that closes issue #6.

--- apps/news/forms.py
+++ apps/news/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
 from apps.news.models import News
-from apps.news.widgets import WYMEditor
+
+def get_editor_widget():
+	from django.conf import settings
+	from cms.plugins.text.settings import USE_TINYMCE
+	if USE_TINYMCE and "tinymce" in settings.INSTALLED_APPS:
+		from apps.news.widgets import TinyMCEEditor
+		return TinyMCEEditor()
+	else:
+		from apps.news.widgets import WYMEditor
+		return WYMEditor()
 
 class NewsAdminModelForm(forms.ModelForm):
-	body = forms.CharField(widget=WYMEditor())
+	body = forms.CharField(widget=get_editor_widget())
 
 	class Meta:
 		model = News
--- apps/news/widgets.py
+++ apps/news/widgets.py
@@ -3,18 +3,39 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 
-from apps.news.settings import NEWS_MEDIA_URL
+class TinyMCEEditor(forms.Textarea):
+	class Media:
+		js = (
+			"js/jquery-1.8.1.min.js",
+			"js/jquery.tinymce.js",
+		)
+
+	def __init__(self, language=None):
+		self.language = language or settings.LANGUAGE_CODE[:2]
+		super(TinyMCEEditor, self).__init__()
+
+	def render(self, name, value, attrs=None):
+		rendered = super(TinyMCEEditor, self).render(name, value, attrs)
+		context = {
+			"name": name,
+			"lang": self.language[:2],
+			"language": self.language,
+			"STATIC_URL": settings.STATIC_URL,
+		}
+
+		return rendered + mark_safe(render_to_string(
+			"admin/news/widgets/tinymce.html", context))
 
 class WYMEditor(forms.Textarea):
 	class Media:
 		js = (
-			'jquery/jquery.js',
-			'news/wymeditor/jquery.wymeditor.pack.js',
+			"js/jquery-1.8.1.min.js",
+			"cms/wymeditor/jquery.wymeditor.pack.js",
 		)
 
 	def __init__(self, language=None, attrs=None):
 		self.language = language or settings.LANGUAGE_CODE[:2]
-		self.attrs = {'class': 'wymeditor'}
+		self.attrs = {"class": "wymeditor"}
 		if attrs:
 			self.attrs.update(attrs)
 		super(WYMEditor, self).__init__(attrs)
@@ -22,14 +43,13 @@ class WYMEditor(forms.Textarea):
 	def render(self, name, value, attrs=None):
 		rendered = super(WYMEditor, self).render(name, value, attrs)
 		context = {
-			'name': name,
-			'lang': self.language[:2],
-			'language': self.language,
-			'NEWS_MEDIA_URL': NEWS_MEDIA_URL,
+			"name": name,
+			"lang": self.language[:2],
+			"language": self.language,
+			"STATIC_URL": settings.STATIC_URL,
+			"page_link_wymeditor": 0,
+			"filebrowser": 0,
 		}
-		context['page_link_wymeditor'] = 0
 
-		context['filebrowser'] = 0
-			
 		return rendered + mark_safe(render_to_string(
-			'admin/news/widgets/wymeditor.html', context))
+			"admin/news/widgets/wymeditor.html", context))
