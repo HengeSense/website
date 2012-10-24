de9fbe66e2ef6a670ea03fe305f665fa8795fc6e
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-10-24 19:24:33 +0200

    User profiles nillable.

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from registration.signals import user_registered
 from django.utils.translation import ugettext_lazy as _
 
 GENDER_CHOICES = (
@@ -10,8 +11,8 @@ GENDER_CHOICES = (
 class UserProfile(models.Model):
 	user = models.ForeignKey(User, unique=True)
 
-	date_of_birth = models.DateField(_("Date of Birth"))
-	gender = models.CharField(_("Gender"), max_length=1, null=False, choices=GENDER_CHOICES)
+	date_of_birth = models.DateField(_("Date of Birth"), blank=True, null=True)
+	gender = models.CharField(_("Gender"), max_length=1, blank=True, null=True, choices=GENDER_CHOICES)
 
 	receive_updates = models.BooleanField(_("Receive updates from this Web site"), default=True)
 
@@ -22,3 +23,7 @@ class UserProfile(models.Model):
 
 	def __unicode__(self):
 		return _("User profile for %s") % self.user.get_full_name()
+
+def user_registered_callback(sender, user, request, **kwargs):
+	UserProfile.objects.get_or_create(user=user)
+user_registered.connect(user_registered_callback)
