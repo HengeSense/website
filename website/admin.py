0f7cddf70f75244f906ac9aca923720d00546f0b
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-11-17 15:50:34 +0100

    Show more UserProfile fields in admin.
    The admin application now shows more information about user
    profiles.

--- website/admin.py
+++ website/admin.py
@@ -4,7 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 from models import *
 
 class UserProfileAdmin(admin.ModelAdmin):
-	list_display = ("user", "gender")
+	list_display = ("user", "gender", "receive_updates")
+	list_filter = ("gender", "receive_updates")
 	list_select_related = True
 	ordering = ("date_of_birth",)
 	search_field = ["user__username", "user__fist_name", "user__last_name", "user__email"]
